608a74881b55cdaa56428a00ac515b6f7bb6ae08
Manuel Gutierrez Moya <manuel.gutierrez.moya@everis.com>
2020-11-14 12:10:39 +0100

    Add rectclerView in MainActivity. Create item to recyclerView.

--- app/src/main/java/com/manugmoya/bicimadstations/ui/MainActivity.kt
+++ app/src/main/java/com/manugmoya/bicimadstations/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.manugmoya.bicimadstations
+package com.manugmoya.bicimadstations.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.manugmoya.bicimadstations.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
