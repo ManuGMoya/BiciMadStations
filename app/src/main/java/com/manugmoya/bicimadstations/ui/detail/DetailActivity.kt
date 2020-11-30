e086348699b5001218d82dbe4751a5a0c5faf85b
Manuel Gutierrez Moya <manuel.gutierrez.moya@everis.com>
2020-11-30 15:14:11 +0100

    Implement MVP in DetailActivity.

--- app/src/main/java/com/manugmoya/bicimadstations/ui/detail/DetailActivity.kt
+++ app/src/main/java/com/manugmoya/bicimadstations/ui/detail/DetailActivity.kt
@@ -7,26 +7,29 @@ import com.manugmoya.bicimadstations.R
 import com.manugmoya.bicimadstations.databinding.ActivityDetailBinding
 import com.manugmoya.bicimadstations.model.Station
 
-class DetailActivity : AppCompatActivity() {
+class DetailActivity : AppCompatActivity() , DetailPresenter.View{
 
     companion object {
         const val STATION = "DetailActivity:station"
     }
 
     private lateinit var binding : ActivityDetailBinding
+    private val presenter = DetailPresenter()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityDetailBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        presenter.onCreate(this, intent.getParcelableExtra<Station>(STATION))
+    }
 
-        intent.getParcelableExtra<Station>(STATION)?.run {
+    override fun updateUI(station: Station?)  {
+        station?.let { mStation ->
+            supportActionBar?.title = mStation.name
 
-            supportActionBar?.title = name
+            binding.stationDetailInfo.setStation(mStation)
 
-            binding.stationDetailInfo.setStation(this)
-
-            when(light){
+            when(mStation.light){
                 0 -> {binding.stationDetailInfo.setBackgroundResource(R.color.green_700)
                     binding.root.setBackgroundResource(R.color.green_700)}
                 1 -> {binding.stationDetailInfo.setBackgroundResource(R.color.orange_700)
@@ -36,15 +39,26 @@ class DetailActivity : AppCompatActivity() {
                 3 -> {binding.stationDetailInfo.setBackgroundResource(R.color.red_700)
                     binding.root.setBackgroundResource(R.color.red_700)}
             }
-            if(freeBases == 0){
+            if(mStation.freeBases == 0){
                 binding.stationDetailInfo.setBackgroundResource(R.color.red_700)
                 binding.root.setBackgroundResource(R.color.red_700)
             }
 
             binding.fab.setOnClickListener {
-                Toast.makeText(this@DetailActivity, name, Toast.LENGTH_LONG).show()
+                Toast.makeText(this@DetailActivity, mStation.name, Toast.LENGTH_LONG).show()
             }
+        } ?: run {
+            Toast.makeText(
+                this,
+                "No ha sido posible recuperar la información de la estación.",
+                Toast.LENGTH_SHORT
+            ).show()
         }
+    }
+
+    override fun onDestroy() {
+        presenter.onDestroy()
+        super.onDestroy()
     }
 
 }
