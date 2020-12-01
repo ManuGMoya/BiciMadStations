b975b1fc267e9e6d2c3980594741f381927a779d
Manuel Gutierrez Moya <manuel.gutierrez.moya@everis.com>
2020-12-01 15:14:29 +0100

    Migrate MainActivity from MVP to MVVM. Refactor MainPresenter to MainViewModel. add ViewModel dependency.

--- app/src/main/java/com/manugmoya/bicimadstations/ui/main/MainActivity.kt
+++ app/src/main/java/com/manugmoya/bicimadstations/ui/main/MainActivity.kt
@@ -2,66 +2,49 @@ package com.manugmoya.bicimadstations.ui.main
 
 import android.os.Bundle
 import android.view.View
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import com.manugmoya.bicimadstations.databinding.ActivityMainBinding
 import com.manugmoya.bicimadstations.model.LocationRepository
-import com.manugmoya.bicimadstations.model.Station
 import com.manugmoya.bicimadstations.model.StationsRepository
 import com.manugmoya.bicimadstations.ui.common.startActivity
 import com.manugmoya.bicimadstations.ui.detail.DetailActivity
+import com.manugmoya.bicimadstations.ui.main.MainViewModel.UiModel.*
 
-class MainActivity : AppCompatActivity(), MainPresenter.View {
+class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var viewModel: MainViewModel
+    private lateinit var adapter: StationsAdapter
 
-    private val presenter by lazy {  MainPresenter( LocationRepository(this), StationsRepository()) }
-
-    private val adapter = StationsAdapter(this) { station ->
-        presenter.onStationClicked(station)
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        presenter.onCreate(this)
+        viewModel = ViewModelProvider(
+            this, MainViewModelFactory(
+                LocationRepository(this),
+                StationsRepository()
+            )
+        )[MainViewModel::class.java]
 
+        adapter = StationsAdapter(this, viewModel::onStationClicked)
         binding.rvStations.adapter = adapter
+
+        viewModel.model.observe(this, Observer (::updateUi))
     }
 
-    override fun showProgress() {
-        binding.progress.visibility = View.VISIBLE
-    }
-
-    override fun hideProgress() {
-        binding.progress.visibility = View.GONE
-    }
-
-
-    override fun updateData(stations: List<Station>?) {
-        stations?.let { stationsList ->
-            adapter.stationsList =
-                stationsList
-        } ?: run {
-            Toast.makeText(
-                this@MainActivity,
-                "Ha habido un error al recuperar los datos",
-                Toast.LENGTH_SHORT
-            ).show()
+    private fun updateUi(model: MainViewModel.UiModel) {
+        binding.progress.visibility = if (model == Loading) View.VISIBLE else View.GONE
+        when (model) {
+            is Content -> adapter.stationsList = model.stations
+            is Navigation -> startActivity<DetailActivity> {
+                putExtra(DetailActivity.STATION, model.station)
+            }
         }
-    }
-
-    override fun navigateToDetail(station: Station) {
-        startActivity<DetailActivity> {
-            putExtra(DetailActivity.STATION, station)
-        }
-    }
-
-    override fun onDestroy() {
-        presenter.onDestroy()
-        super.onDestroy()
     }
 
 }
---
+++ app/src/main/java/com/manugmoya/bicimadstations/ui/main/MainViewModel.kt
@@ -0,0 +1,66 @@
+package com.manugmoya.bicimadstations.ui.main
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.manugmoya.bicimadstations.model.LocationRepository
+import com.manugmoya.bicimadstations.model.Station
+import com.manugmoya.bicimadstations.model.StationsRepository
+import com.manugmoya.bicimadstations.ui.common.Scope
+import com.manugmoya.bicimadstations.ui.common.orderListByLocation
+import kotlinx.coroutines.async
+import kotlinx.coroutines.launch
+
+class MainViewModel(
+    private val locationRepository: LocationRepository,
+    private val stationsRepository: StationsRepository
+) : ViewModel() ,Scope by Scope.Impl() {
+
+    sealed class UiModel{
+        object Loading : UiModel()
+        class Content(val stations : List<Station>) : UiModel()
+        class Navigation(val station: Station) : UiModel()
+    }
+
+    private val _model = MutableLiveData<UiModel>()
+    val model : LiveData<UiModel>
+        get() {
+            if(_model.value == null) refresh()
+            return _model
+        }
+
+    init {
+        initScope()
+    }
+
+    private fun refresh() {
+        launch {
+            _model.value = UiModel.Loading
+            val location = async { locationRepository.getLocation()}
+            val stations = async { stationsRepository.getDataStations() }
+            _model.value = stations.await()?.orderListByLocation(location.await())?.let {
+                UiModel.Content(
+                    it
+                )
+            }
+        }
+    }
+
+    fun onStationClicked(station: Station) {
+        _model.value = UiModel.Navigation(station)
+    }
+
+    override fun onCleared() {
+        cancelScope()
+    }
+}
+
+@Suppress("UNCHECKED_CAST")
+class MainViewModelFactory(private val locationRepository: LocationRepository, private val stationsRepository: StationsRepository) :
+    ViewModelProvider.Factory {
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T =
+        MainViewModel(locationRepository,stationsRepository) as T
+
+
+}
