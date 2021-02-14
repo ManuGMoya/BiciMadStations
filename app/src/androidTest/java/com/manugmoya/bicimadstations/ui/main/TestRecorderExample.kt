516bd6f4ec3cfbabd56bc27b567fa41de0835187
Manuel Gutierrez Moya <manuel.gutierrez.moya@everis.com>
2021-02-14 14:09:36 +0100

    Configure MockWebServer.

--- app/src/androidTest/java/com/manugmoya/bicimadstations/ui/main/TestRecorderExample.kt
+++ app/src/androidTest/java/com/manugmoya/bicimadstations/ui/main/TestRecorderExample.kt
@@ -4,10 +4,10 @@ package com.manugmoya.bicimadstations.ui.main
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
 import androidx.test.rule.ActivityTestRule
 import androidx.test.rule.GrantPermissionRule
-import androidx.test.runner.AndroidJUnit4
 import com.manugmoya.bicimadstations.R
 import org.hamcrest.Matchers.allOf
 import org.junit.Rule
